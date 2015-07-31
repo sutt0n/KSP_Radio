991f009f25bd396c9215e76fab1dcdba03d5912d
Joseph Sutton <joe@c2cschools.com>
2015-07-31 07:36:21 -0500

    First window creation; implemented Logger (simple for now)

---
+++ KSP_Radio/Logger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace KSP_Radio
+{
+	public class Logger : MonoBehaviour
+	{
+
+		public enum Severity {
+			Notice = 0,
+			Warning = 1,
+			Fatal = 2
+		};
+
+		public static void Log(string message, Severity level = Severity.Notice) {
+
+			string severity;
+
+			switch (level) {
+			case Severity.Warning:
+				severity = "[SEVERE]";
+				break;
+			
+			default:
+			case Severity.Notice:
+				severity = "[NOTICE]";
+				break;
+
+			case Severity.Fatal:
+				severity = "[FATAL]";
+				break;
+			}
+
+			print("[KSP Radio] " + severity + ": " + message);
+
+		}
+
+	}
+}
+
---
+++ KSP_Radio/Radio.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace KSP_Radio
+{
+	[KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+	public class Radio : MonoBehaviour
+	{
+
+		private static Rect winPosition = new Rect (0, 0, 320, 240);
+		private static GUIStyle winStyle = null;
+		private static Boolean btnState = false;
+
+		public void Awake() {
+			RenderingManager.AddToPostDrawQueue (0, OnDrawInterface);
+		}
+
+		private void OnDrawInterface() {
+			// init window 
+			winPosition = GUI.Window (
+				1337, 
+				winPosition, 
+				_initWindow, 
+				"KSP Radio", 
+				winStyle
+			);
+		}
+
+		private void _initWindow(int windowId) {
+			GUILayout.BeginHorizontal();
+			GUILayout.Label ("ABC-");
+			GUILayout.Label ("123");
+			GUILayout.EndHorizontal();
+
+			GUILayout.BeginHorizontal();
+			btnState = GUILayout.Toggle (btnState, "Button state: " + btnState);
+
+			GUI.DragWindow();
+		}
+
+		/**
+		 * Method is called upon the first
+		 * 
+		 * @return void
+		 */ 
+		protected void Start() {
+
+			Logger.Log ("KSP Radio initialized.");
+
+			// init window style
+			winStyle = new GUIStyle (HighLogic.Skin.window);
+
+		}
+	}
+}
+
